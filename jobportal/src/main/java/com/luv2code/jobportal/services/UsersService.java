90c41f93c6d4722f02ab0dda7a703fcfc4558cb9
Abdelrhman <abdo.mohsen3457643@gmail.com>
2024-10-02 02:45:02 +0200

    profiles repo testing

--- jobportal/src/main/java/com/luv2code/jobportal/services/UsersService.java
+++ jobportal/src/main/java/com/luv2code/jobportal/services/UsersService.java
@@ -1,7 +1,11 @@
 package com.luv2code.jobportal.services;
 
 
+import com.luv2code.jobportal.entity.JobSeekerProfile;
+import com.luv2code.jobportal.entity.RecruiterProfile;
 import com.luv2code.jobportal.entity.Users;
+import com.luv2code.jobportal.repository.JobSeekerProfileRepository;
+import com.luv2code.jobportal.repository.RecruiterProfileRepository;
 import com.luv2code.jobportal.repository.UsersRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,14 +16,29 @@ import java.util.Date;
 public class UsersService {
 
     private final UsersRepository usersRepository;
+    private final RecruiterProfileRepository recruiterProfileRepository;
+    private final JobSeekerProfileRepository jobSeekerProfileRepository;
 
     @Autowired
-    public UsersService(UsersRepository usersRepository) {
+    public UsersService(UsersRepository usersRepository,
+                        JobSeekerProfileRepository  jobSeekerProfileRepository,
+                        RecruiterProfileRepository recruiterProfileRepository) {
+
+
         this.usersRepository = usersRepository;
+        this.recruiterProfileRepository = recruiterProfileRepository;
+        this.jobSeekerProfileRepository = jobSeekerProfileRepository;
     }
     public Users addNew(Users users) {
         users.setActive(true);
         users.setRegistrationDate(new Date(System.currentTimeMillis()));
-        return usersRepository.save(users);
+        Users savedUser = usersRepository.save(users);
+        int userTypeId = users.getUserTypeId().getUserTypeId();
+        if(userTypeId== 1 ){
+             recruiterProfileRepository.save(new RecruiterProfile(savedUser));
+        }else {
+             jobSeekerProfileRepository.save(new JobSeekerProfile(savedUser));
+        }
+        return savedUser;
     }
 }
